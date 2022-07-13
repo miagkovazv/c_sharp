7db726da30415172b410dd192d7eab3d3792877c
zoya.miagkova <myagk.zoya2016@yandex.ru>
2022-07-13 22:50:26 +0300

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a%b ==0)
+     Console.WriteLine("Кратно");
+else  
+{ Console.Write("Не кратно, остаток ");
+Console.WriteLine(a%b);}
+
+
+
