96b3abfce6ca714690b7d02ae134b3d1c1f7128b
zoya.miagkova <myagk.zoya2016@yandex.ru>
2022-07-13 23:04:23 +0300

    task5

---
+++ task5/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите два числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a*a == b)
+Console.WriteLine("да");
+else Console.WriteLine("нет");
+
